217891b46893dd9037768011f43cd4501230cc00
Kong Lee <chicongxq2610@gmail.com>
2023-05-22 10:35:22 +0700

    fix: cors issue

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,9 +1,9 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
 
-import bcrypt from "bcryptjs";
-import User from "../models/User";
-import { CustomError } from "../utils/custom-error";
-import jwt from "jsonwebtoken";
+import bcrypt from 'bcryptjs';
+import User from '../models/User';
+import { CustomError } from '../utils/custom-error';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (
   req: Request,
@@ -15,7 +15,7 @@ export const signup = async (
     const hash = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({ ...req.body, password: hash });
     await newUser.save();
-    res.status(200).send("User has been created!");
+    res.status(200).send('User has been created!');
   } catch (err) {
     next(err);
   }
@@ -27,17 +27,17 @@ export const signin = async (
   next: NextFunction
 ) => {
   try {
-    const user = await User.findOne({ name: req.body.name });
-    if (!user) return next(new CustomError("User not found!", 404));
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return next(new CustomError('User not found!', 404));
 
     const isValid = await bcrypt.compare(req.body.password, user.password);
-    if (!isValid) return next(new CustomError("Wrong Credentials", 400));
+    if (!isValid) return next(new CustomError('Wrong Credentials', 400));
 
     const { password, ...userWithoutPassword } = user.toJSON();
     const token = jwt.sign({ id: user._id }, process.env.JWT as string);
 
     res
-      .cookie("access_token", token, { httpOnly: true })
+      .cookie('access_token', token, { httpOnly: true })
       .status(200)
       .json(userWithoutPassword);
   } catch (err) {
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,32 @@
-import express, { NextFunction, Request, Response } from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRouter from "./routes/user";
-import authRouter from "./routes/auth";
-import { CustomError } from "./utils/custom-error";
-import videoRouter from "./routes/video";
-import cookieParser from "cookie-parser";
+import express, { NextFunction, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user';
+import authRouter from './routes/auth';
+import { CustomError } from './utils/custom-error';
+import videoRouter from './routes/video';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 dotenv.config();
 
 const connect = () => {
   mongoose.connect(process.env.MONGO as string).then(() => {
-    console.log("Connected to db");
+    console.log('Connected to db');
   });
 };
+
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-app.use("/api/users", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/videos", videoRouter);
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/videos', videoRouter);
 
 app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
   const status = err.status || 500;
-  const message = err.message || "Something went wrong!";
+  const message = err.message || 'Something went wrong!';
   return res.status(status).json({
     success: false,
     status,
@@ -33,5 +36,5 @@ app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
 
 app.listen(8800, () => {
   connect();
-  console.log("Connect to Server");
+  console.log('Connect to Server');
 });
